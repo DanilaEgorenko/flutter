e6ff4d923a4dcb8eb3e52f3847004aa5d4298130
Danila <danila_egorenko@icloud.com>
2023-12-14 15:41:00 +0300

    fix

--- lib/cubits/authentication_cubit.dart
+++ lib/cubits/authentication_cubit.dart
@@ -1,23 +1,13 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-enum AuthenticationStatus { authenticated, unauthenticated }
-
-class User {
-  final String name;
-  final String password;
-
-  User({required this.name, required this.password});
-}
-
 class AuthenticationCubit extends Cubit<AuthenticationStatus> {
+  User? _currentUser;
+
   AuthenticationCubit() : super(AuthenticationStatus.unauthenticated) {
     loadUserFromPrefs();
   }
-
-  User? _currentUser;
 
   User? get currentUser => _currentUser;
 
@@ -34,23 +24,6 @@ class AuthenticationCubit extends Cubit<AuthenticationStatus> {
     }
   }
 
-  Future<void> loginUser(User user, BuildContext context) async {
-    if (user.name == 'admin' && user.password == 'password') {
-      await _saveUserToPrefs(user);
-
-      emit(AuthenticationStatus.authenticated);
-    } else {
-      _showErrorMessage(context, 'Wrong username or password');
-    }
-  }
-
-  void logoutUser() {
-    _currentUser = null;
-    _removeUserFromPrefs();
-
-    emit(AuthenticationStatus.unauthenticated);
-  }
-
   Future<void> loadUserFromPrefs() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     final name = prefs.getString('user_name');
@@ -61,9 +34,31 @@ class AuthenticationCubit extends Cubit<AuthenticationStatus> {
       _currentUser = User(name: name, password: password);
       emit(AuthenticationStatus.authenticated);
     } else {
-
       emit(AuthenticationStatus.unauthenticated);
     }
+  }
+
+  Future<void> loginUser(User user, BuildContext context) async {
+    if (user.name == 'cat' && user.password == 'admin') {
+      await _saveUserToPrefs(user);
+
+      emit(AuthenticationStatus.authenticated);
+    } else {
+      _showErrorMessage(context, 'Неправильный логин или пароль');
+    }
+  }
+
+  void logoutUser() {
+    _currentUser = null;
+    _removeUserFromPrefs();
+
+    emit(AuthenticationStatus.unauthenticated);
+  }
+
+  Future<void> _removeUserFromPrefs() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.remove('user_name');
+    prefs.remove('user_password');
   }
 
   Future<void> _saveUserToPrefs(User user) async {
@@ -71,12 +66,6 @@ class AuthenticationCubit extends Cubit<AuthenticationStatus> {
     prefs.setString('user_name', user.name);
     prefs.setString('user_password', user.password);
     prefs.setBool('is_authenticated', true);
-  }
-
-  Future<void> _removeUserFromPrefs() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.remove('user_name');
-    prefs.remove('user_password');
   }
 
   void _showErrorMessage(BuildContext context, String message) {
@@ -87,4 +76,13 @@ class AuthenticationCubit extends Cubit<AuthenticationStatus> {
       ),
     );
   }
+}
+
+enum AuthenticationStatus { authenticated, unauthenticated }
+
+class User {
+  final String name;
+  final String password;
+
+  User({required this.name, required this.password});
 }
